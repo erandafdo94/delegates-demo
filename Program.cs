fa1d71af90f68c9b6193f87451b90ef49ceec6f4
effdog <selfnba@gmail.com>
2024-08-06 03:32:40 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public delegate void CallbackDelegate(string message);
+
+public class Program
+{
+    public static void ShowMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public static void ProcessMessage(string message, CallbackDelegate callback)
+    {
+        // Processing message...
+        callback(message);
+    }
+
+    public static void Main()
+    {
+        CallbackDelegate callback = new CallbackDelegate(ShowMessage);
+        ProcessMessage("Hello, Delegate!", callback);
+    }
+}
